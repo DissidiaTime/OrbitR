d2a1b5448655c3f34e34ea1b996842bd9000240d
Serah E <112106837+DissidiaTime@users.noreply.github.com>
2024-04-28 13:16:55 -0400

    Delete tests/testthat directory

--- tests/testthat/test-get_planet_position.R
+++
@@ -1,8 +0,0 @@
-library(testthat)
-library(OrbitR)
-
-test_that("Planet position calculation is correct", {
-  result <- get_planet_position(1, 0, 0)
-  expect_equal(result$x, 1)
-  expect_equal(result$y, 0)
-})
